451219a2fd71a5f3a0eea6623ad09cb5b5af5465
chencat <chencatpost@gmail.com>
2023-02-24 09:39:53 +0800

    backup

--- yoga.js
+++ yoga.js
@@ -1,7 +1,50 @@
-var host = 'https://e873-59-126-54-247.jp.ngrok.io'
+var host = 'localhost'
 
 moment.locale('zh-tw', {
     week: {
         dow: 1
     }
 });
+
+function initApp(callback) {
+    let liffID = '1657127222-vlpYeZjK';
+
+    liff.init({
+        liffId: liffID
+    }).then(function () {
+        console.log('LIFF init');
+
+        // 這邊開始寫使用其他功能
+
+        // login call, only when external browser or LINE's in-app browser is used
+        if (!liff.isLoggedIn()) {
+            liff.login();
+        } else {
+
+            // window.location.href = 'class.html';
+
+            // fetchUser();
+
+        }
+    }).catch(function (error) {
+        console.log(error);
+        liff.logout();
+    });
+}
+
+function fetchUser(callbacdk) {
+
+    const token = liff.getIDToken();
+    axios.get(`${host}/ccdartstudio/me`, {
+        headers: {
+            'Authorization': `${token}`
+        }
+    })
+        .then(function (response) {
+            console.log(response)
+            callbacdk(response.data.obj)
+        }).catch(function (error) {
+            console.log(error);
+            callbacdk(null)
+        });
+}
